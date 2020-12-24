b3db41c0311a00414daf011120446036c4e918cf
Valentin Famelart <valentin.famelart@invenis.co>
2020-12-24 11:15:28 +0100

    Add day 6

---
+++ 06/code.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from typing import List, Optional
+import re
+
+
+def parse_input(file_name: str):
+    return None
+
+
+def part_1(forms: List):
+    """
+    Expected answer: 6521.
+    """
+    return None
+
+
+def part_2(forms: List):
+    """
+    Expected answer: xxx.
+    """
+    return None
+
+
+if __name__ == "__main__":
+    forms = parse_input("rules.txt")
+
+    res_1 = part_1(forms)
+    print("The answer for part 1 is:", res_1)
+
+    res_2 = part_2(forms)
+    print("The answer for part 2 is:", res_2)
